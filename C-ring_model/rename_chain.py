e009855c7077e723426f2e8977fa47aad29e7b29
Louie Henderson <l.henderson15@imperial.ac.uk>
2020-06-17 10:27:46 +0100

    Added FliIH model/chain ID script

---
+++ C-ring_model/rename_chain.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import Bio.PDB
+from Bio import PDB
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Rename protein chains')
+
+#Adds parser arguments which the user will input
+parser.add_argument('-p', '--pdb', type=str, help='Enter pdb to rename chains (lower case only)', required = True)
+parser.add_argument('-o', '--out', type=str, help='Output name of altered pdb', required = True)
+parser.add_argument('-c', '--chain', type=str, help='Enter chain identifier to start new chain ID')
+
+#Associates arguments to objects
+args = parser.parse_args()
+pdb_input    = args.pdb
+pdb_output   = str(args.out)
+chain_id     = args.chain
+
+#Read pdb file into structure object
+def read_pdb(pdb_input):
+
+    #Creates structure object of protein to be tagged
+    parser = PDB.PDBParser(PERMISSIVE=1)
+    pdb_change = parser.get_structure(pdb_input,"./"+pdb_input+".pdb")
+
+    return pdb_change
+
+#Run program
+Init_PDB = read_pdb(pdb_input)
+
+id_len = len(Init_PDB.get_list()[0].get_list())
+
+# Create alphabet list of lowercase letters
+alphabet = []
+
+for letter in range(65, 91):
+    alphabet.append(chain_id + chr(letter))
+
+for letter in range(97,123):
+    alphabet.append(chain_id + chr(letter))
+
+#Rename chain ids
+for chain, new_id in zip(Init_PDB.get_chains(), alphabet):
+    chain.id = new_id
+
+#print pdb_output
+print "Complete!"
+
+#Saves the PDB of the optimal final acceptor
+io=Bio.PDB.PDBIO()
+io.set_structure(Init_PDB)
+io.save(".pdb")
