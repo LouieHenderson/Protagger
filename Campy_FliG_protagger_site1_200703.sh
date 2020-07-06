b99bcc4632706d651c970a8857a435c9926f2033
Louie <l.henderson15@imperial.ac.uk>
2020-07-06 14:07:51 +0100

    Added Campy FliG tags

--- Campy_FliG_protagger_site1_200703.sh
+++ Campy_FliG_protagger_site1_200703.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#SBATCH --ntasks=10
+#SBATCH --ntasks=10 --nodelist=heartofgold2
 
-./bin/runProtagger.py -b ./FliG_1tag -o Campy_FliG_site1_200703 -p Campy_FliGM_cring.pdb -n 255 -c 262 -r 4 -u 10 -m 80 -t 2.5 -v 3 -x I,J,K,L,M -l > Campy_FliG_site1_10core_o3_200703.log
-
-mv Campy_FliG_site1_10core_o3_200703.log ./Campy_FliG_site1_200703
+./bin/runProtagger.py -b ./FliG_1tag -o Campy_FliG_site1_200703 -p Campy_FliGM_cring -n 251 -c 279 -r 3 -u 10 -m 80 -t 2.5 -v 3 -x I,J,K,L,M -l -d > Campy_FliG_site1_10core_o3_200703.log
--- Campy_FliG_protagger_site2_200703.sh
+++ Campy_FliG_protagger_site2_200703.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#SBATCH --ntasks=10
+#SBATCH --ntasks=10 --nodelist=heartofgold2
 
-./bin/runProtagger.py -b ./FliG_1tag -o Campy_FliG_site2_200703 -p Campy_FliGM_cring.pdb -n 221 -c 229 -r 4 -u 10 -m 80 -t 2.5 -v 3 -x I,J,K,L,M -l > Campy_FliG_site2_10core_o3_200703.log
-
-mv Campy_FliG_site2_10core_o3_200703.log ./Campy_FliG_site2_200703
+./bin/runProtagger.py -b ./FliG_1tag -o Campy_FliG_site2_200703 -p Campy_FliGM_cring -n 229 -c 238 -r 3 -u 10 -m 80 -t 2.5 -v 3 -x I,J,K,L,M -l > Campy_FliG_site2_10core_o3_200703.log
