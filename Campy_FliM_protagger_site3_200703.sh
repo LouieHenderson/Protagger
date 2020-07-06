575d45bc565e5ba2e8130c767bd4f52711f27c84
Louie Henderson <l.henderson15@imperial.ac.uk>
2020-07-06 14:20:33 +0100

    Minor change

--- Campy_FliM_protagger_site3_200703.sh
+++ Campy_FliM_protagger_site3_200703.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --ntasks=10
 
-./bin/runProtagger.py -b ./FliM_1tag -o FliM_multimer_site3_200703 -p Campy_FliGM_cring -n 86 -c 91 -r 4 -u 10 -m 80 -t 2.5 -v 3 -x W,X,Y,Z,a -l > Campy_FliM_site3_10core_o3_200703.log
+./bin/runProtagger.py -b ./FliM_1tag -o Campy_FliM_site3_200706 -p Campy_FliGM_cring -n 76 -c 101 -r 3 -u 10 -m 80 -t 2.5 -v 3 -x W,X,Y,Z,a -l -d > Campy_FliM_site3_10core_o3_200706.log
 
-mv Campy_FliM_site3_10core_o3_200703.log ./FliM_multimer_site3_200703
+mv Campy_FliM_site3_10core_o3_200706.log ./Campy_FliM_site3_200706
