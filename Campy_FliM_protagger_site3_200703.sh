bff1cd4c5599ac0df0fc56863f4261860cb2418e
Louie <l.henderson15@imperial.ac.uk>
2020-07-06 17:06:03 +0100

    Found new FliM campy tag

--- Campy_FliM_protagger_site3_200703.sh
+++ Campy_FliM_protagger_site3_200703.sh
@@ -2,5 +2,3 @@
 #SBATCH --ntasks=10
 
 ./bin/runProtagger.py -b ./FliM_1tag -o Campy_FliM_site3_200706 -p Campy_FliGM_cring -n 76 -c 101 -r 3 -u 10 -m 80 -t 2.5 -v 3 -x W,X,Y,Z,a -l -d > Campy_FliM_site3_10core_o3_200706.log
-
-mv Campy_FliM_site3_10core_o3_200706.log ./Campy_FliM_site3_200706
