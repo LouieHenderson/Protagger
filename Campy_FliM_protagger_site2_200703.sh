9ce3defd7ef54eeb9202eb66eb446f094dde3671
Louie <l.henderson15@imperial.ac.uk>
2020-07-07 11:08:58 +0100

    Minor changes

---
+++ Campy_FliM_protagger_site2_200703.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#SBATCH --ntasks=10
+
+./bin/runProtagger.py -b ./database_200625 -o Campy_FliM_site2_200706 -p Campy_FliGM_cring -n 118 -c 131 -r 3 -u 10 -m 80 -t 2.5 -v 3 -x W,X,Y,Z,a -l -d > Campy_FliM_site2_10core_o3_200706.log
