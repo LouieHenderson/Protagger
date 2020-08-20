cd843755255881c1eaffbb34b09291d4d1ea1238
Louie Henderson <l.henderson15@imperial.ac.uk>
2020-08-20 13:36:11 +0100

    Added FliGn tag sites

---
+++ C-ring_model/Campy_C-ring/makecring_FliGn.sh
@@ -0,0 +1,17 @@
+#FliG chain IDs I,J,K,L,M
+#FliG aligned based on FliGc domain
+
+../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliGn -c 6 -d 105 -e 1 -f 100 -x I
+../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliGn -c 6 -d 105 -e 1 -f 100 -x J
+../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliGn -c 6 -d 105 -e 1 -f 100 -x K
+../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliGn -c 6 -d 105 -e 1 -f 100 -x L
+../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliGn -c 6 -d 105 -e 1 -f 100 -x M
+
+#FliM chain IDs W,X,Y,Z,a
+
+#../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliM_par -c 49 -d 228 -e 51 -f 230 -x W
+#../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliM_par -c 49 -d 228 -e 51 -f 230 -x X
+#../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliM_par -c 49 -d 228 -e 51 -f 230 -x Y
+#../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliM_par -c 49 -d 228 -e 51 -f 230 -x Z
+#../../bin/align_domains.py  -a C-ring_portion_15mer -b Campy_FliM_par -c 49 -d 228 -e 51 -f 230 -x a
+
