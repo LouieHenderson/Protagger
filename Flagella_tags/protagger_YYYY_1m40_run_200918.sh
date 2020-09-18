f34a2b6f82fd59773b24fda21d85059ace166c67
Louie <l.henderson15@imperial.ac.uk>
2020-09-18 16:34:20 +0100

    1m40 added

---
+++ Flagella_tags/protagger_YYYY_1m40_run_200918.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#SBATCH --ntasks=50 --partition=heartofgoldCPU
+
+../bin/runProtagger.py -b ../../../tagdatabase_1tag/ -o YYYY_1m40_200918 -p YYYY -n 205 -c 212 -r 3 -u 50 -t 2.5 -v 4 -l > protagger_YYYY_1m40_db_200918.log
+
